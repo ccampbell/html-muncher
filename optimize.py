99f04d02cd2e49ca9617bec05bedca00b43c9229
Craig Campbell <iamcraigcampbell@gmail.com>
2010-07-25 22:52:40 -0400

    updating to use new modules structure

--- optimize.py
+++ optimize.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from optimizer import VarFactory, Config, Optimizer, OptimizerSingleFile, Util
+from modules.optimizer import Config, Optimizer, OptimizerSingleFile
 
 config = Config()
 config.processArgs()
