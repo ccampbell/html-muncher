b174df2caf7754110f075f60027f9982e49cf0b0
Craig Campbell <iamcraigcampbell@gmail.com>
2010-07-27 23:47:41 -0400

    adding some more code to reducer but it is still buggy with style inheritence

--- modules/reducer.py
+++ modules/reducer.py
@@ -17,25 +17,82 @@ class ChainReducer(object):
         self.config = config
         self.chains = {}
 
+    def findChains(self):
+        for selector in self.getAllCssSelectors():
+            self.addChain(selector)
+        
+        for selector in self.getAllJsSelectors():
+            self.addChain(selector)
+
     def addChain(self, chain):
         if ":" in chain:
             return
-        if chain not in self.chains:
-            self.chains[chain] = "." + VarFactory.getNext("class")
+        chains = chain.split(",")
+        for chain in chains:
+            bits = chain.split(" ")
+        
+            if len(bits) > 1 and chain not in self.chains:
+                self.chains[chain] = "." + VarFactory.getNext("class")
 
-    def findChains(self):
-        css = Util.fileGetContents(self.config.single_file_opt_path)
-        blocks = Optimizer.getCssBlocks(css)
-        for block in blocks:
+    def getCssBlocks(self):
+        css_blocks = []
+        for file in self.config.getOptimizedViewFiles():
+            css = Util.fileGetContents(file)
+            blocks = Optimizer.getCssBlocks(css)
+            for block in blocks:
+                css_blocks.append(block)
+        
+        return css_blocks
+    
+    def getAllJsSelectors(self):
+        js_selectors = []
+        for file in self.config.getOptimizedViewFiles():
+            js = Util.fileGetContents(file)
+            selectors = Optimizer.getJsSelectors(js)
+            for selector in selectors:
+                js_selectors.append(selector[1])
+        
+        return js_selectors
+    
+    def getAllCssSelectors(self):
+        css_selectors = []
+        for block in self.getCssBlocks():
             lines = block.split("\n")
             for line in lines:
                 if "{" in line:
-                    self.addChain(line.lstrip(" ").rstrip("{").rstrip(" "))
+                    css_selectors.append(line.lstrip(" ").rstrip("{").rstrip(" "))
+        
+        return css_selectors
+            
+    def replaceViews(self):
+        for file in self.config.getOptimizedViewFiles():
+            html = Util.fileGetContents(file)
+            new_html = self.updateClasses(html)
+            Util.filePutContents(file, new_html)
+
+    def updateClasses(self, html):
+        d = pq(html)
+        for key, value in self.chains.items():
+            d(key).addClass(value.replace(".", ""))
+        return d.html()
 
     def run(self):
+        print "only single file mode at moment"
         pass
 
 class ChainReducerSingleFile(ChainReducer):
+    def replaceCssAndJavascript(self):
+        contents = Util.fileGetContents(self.config.single_file_opt_path)
+        for key, value in self.chains.items():
+            contents = contents.replace(key + " {", value + " {")
+            contents = contents.replace("(\"" + key + "\")", "(\"" + value + "\")")
+            contents = contents.replace("('" + key + "')", "('" + value + "')")
+
+        Util.filePutContents(self.config.single_file_opt_path, contents)
+        
     def run(self):
-        self.findChains()
-        print self.chains
+        print "reducer is mad buggy ... skipping for now"
+        # self.findChains()
+        # self.replaceCssAndJavascript()
+        # self.replaceViews()
+            
