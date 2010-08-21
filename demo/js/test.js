f2f1f98c343c0cb49db4f9f2c1c03498836d156d
Craig Campbell <iamcraigcampbell@gmail.com>
2010-08-21 12:29:03 -0400

    adding other weird query selectors to demo

--- demo/js/test.js
+++ demo/js/test.js
@@ -20,4 +20,8 @@ window.onload = function()
     var class_thing = $('class_thing');
     class_thing.addClass(test, "whatever");
     class_thing.removeClass(test, "whatever");
+    $.qs(".dont_know", class_thing).value;
+    var cool = $.qs("#one_id.class_thing", test);
+    var another_weird_thing = $.qs(".class1.class2 #another_id");
+    $.qs(".selector1 > .selector2 .selector3");
 }
